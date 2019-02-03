53587732444204996690c8c66b7d7cfb498fd390
makiryk <mira0516.milk@gmail.com>
2019-02-03 15:45:21 +0900

    Initial development

---
+++ BRBouyomi/src/Bouyomi.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using FNF.BouyomiChanApp;
+using FNF.Utility;
+using System.Reflection;
+
+namespace Bouyomi
+{
+    /// <summary>
+    /// 棒読みちゃんタスククラス
+    /// </summary>
+    [Serializable]
+    public class BouyomiTalks
+    {
+        /// <summary>
+        /// コマンド（ 0:メッセージ読み上げ）
+        /// </summary>
+        public Int16 Command { get; set; }
+        /// <summary>
+        /// 速度（-1:棒読みちゃん画面上の設定）
+        /// </summary>
+        public Int16 Speed { get; set; }
+        /// <summary>
+        /// 音程（-1:棒読みちゃん画面上の設定）
+        /// </summary>
+        public Int16 Tone { get; set; }
+        /// <summary>
+        /// 音量（-1:棒読みちゃん画面上の設定）
+        /// </summary>
+        public Int16 Volume { get; set; }
+        /// <summary>
+        /// 声質 0:棒読みちゃん画面上の設定、1:女性1、2:女性2、3:男性1、4:男性2、5:中性、6:ロボット、7:機械1、8:機械2、10001～:SAPI5）
+        /// </summary>
+        public Int16 Voice { get; set; }
+
+        public MessageProperty MessageProp { get; private set; }
+
+        [Serializable]
+        public class MessageProperty
+        {
+            /// <summary>
+            /// 読み上げ文字列
+            /// </summary>
+            public string Message { get; private set; }
+            /// <summary>
+            /// 読み上げ文字列のbyte配列の文字コード(0:UTF-8, 1:Unicode, 2:Shift-JIS)
+            /// </summary>
+            public byte Code { get; private set; }
+            /// <summary>
+            /// 読み上げ文字列のbyte配列の長さ
+            /// </summary>
+            public Int32 Length { get; private set; }
+            /// <summary>
+            /// 読み上げ文字列のbyte配列
+            /// </summary>
+            public byte[] ByteMessage { get; private set; }
+
+            public enum MessageEncode : byte
+            {
+                UTF8 = 0,
+                Unicode = 1,
+                ShiftJIS = 2
+            }
+
+            public MessageProperty(byte bCode, Int32 iLength, byte[] bMessage)
+            {
+                Code = bCode;
+                Length = iLength;
+                ByteMessage = bMessage;
+
+                Message = GetEncoding((MessageEncode)bCode).GetString(bMessage);
+            }
+
+            public MessageProperty(string message, MessageEncode messageEncode)
+            {
+                Message = message;
+
+                Code = (byte)messageEncode;
+                ByteMessage = GetEncoding(messageEncode).GetBytes(message);
+                Length = ByteMessage.Length;
+            }
+
+            private Encoding GetEncoding(MessageEncode bCode)
+            {
+                switch (bCode)
+                {
+                    case MessageEncode.UTF8:
+                        return Encoding.UTF8;
+                    case MessageEncode.Unicode:
+                        return Encoding.Unicode;
+                    case MessageEncode.ShiftJIS:
+                        return Encoding.GetEncoding("Shift_JIS");
+                    default:
+                        return Encoding.UTF8;
+                }
+            }
+        }
+
+        public BouyomiTalks(BinaryReader binaryReader)
+        {
+            Command = binaryReader.ReadInt16();
+            Speed = binaryReader.ReadInt16();
+            Tone = binaryReader.ReadInt16();
+            Volume = binaryReader.ReadInt16();
+            Voice = binaryReader.ReadInt16();
+            var bCode = binaryReader.ReadByte();
+            var iLength = binaryReader.ReadInt32();
+            var bMessage = binaryReader.ReadBytes(iLength);
+            MessageProp = new MessageProperty(bCode, iLength, bMessage);
+        }
+
+        public void AddTalkTask()
+        {
+            Pub.AddTalkTask(MessageProp.Message, Speed, Tone, Volume, (VoiceType)Voice);
+        }
+        
+        public BouyomiTalks ReplacedMessage(string message, MessageProperty.MessageEncode encode)
+        {
+            var clone = this.DeepCopy();
+            clone.SetMessage(message, encode);
+            return clone;
+        }
+
+        public void SetMessage(string message, MessageProperty.MessageEncode messageEncode)
+        {
+            MessageProp = new MessageProperty(message, messageEncode);
+        }
+    }
+
+    /// <summary>
+    /// ReplaceTagクラス
+    /// </summary>
+    public class ReplaceTag
+    {
+        public byte Priority { get; private set; }
+        public string SearchTrigger { get; private set; }
+        public string BeforeReplace { get; private set; }
+        public string AfterReplace { get; private set; }
+
+        public const string SEPARATOR = "\t";
+
+        public ReplaceTag(string line)
+        {
+            var fields = line.Split('\t');
+            Priority = byte.Parse(fields[0]);
+            SearchTrigger = fields[1];
+            BeforeReplace = fields[2];
+            AfterReplace = fields[3];
+        }
+
+    }
+
+    /// <summary>
+    /// 拡張メソッドクラス
+    /// </summary>
+    public static class Extensions
+    {
+        public static T DeepCopy<T>(this T src)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, src);
+                memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+                return (T)binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+
+        /// <summary>
+        /// すべての公開プロパティの情報を文字列にして返します
+        /// </summary>
+        public static string ToStringProperty<T>(this T obj, string separator)
+        {
+            return string.Join(separator, (string[])obj
+                .GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(c => c.CanRead)
+                .Select(c => c.GetValue(obj, null).ToString()).ToArray());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # BRBouyomi
+[棒読みちゃん](http://chi.usamimi.info/Program/Application/BouyomiChan/)でタグ辞書に登録した単語で改行するプラグイン。  
+  
+## Description
+棒読みちゃん専用プラグイン。  
+棒読みちゃん読み上げタスクを受信し、メッセージをReplaceTag.dicに登録された単語で切り分け、棒読みちゃん読み上げタスクに分割登録します。  
+  
+主に以下の状況改善を想定。  
+[Voiceroid Talk Plus](https://ch.nicovideo.jp/Wangdora/blomaga/ar12646)使用時、再生タグ(Sound/SoundW)を発言に含めるとVoiceroidではなく棒読みちゃんでの読み上げになってしまう。  
+(exVoiceとVoiceroidを共存させたかった。)  
+  
+## Demo
+読み上げ「こんにちは今日も良い天気ですね。」
+- タグ辞書
+   - 探索文字列：こんにちは  
+   - 置換後：(soundW こんにちは.wav)  
+  
+#### 通常
+こんにちは今日も良い天気ですね。  
+###### 読み上げ内容
+>(soundW こんにちは.wav)今日も良い天気ですね。  
+  
+#### 切り分け後
+こんにちは  
+今日も良い天気ですね。  
+###### 読み上げ内容
+>(soundW こんにちは.wav)  
+>今日も良い天気ですね。  
+  
+## Usage
+棒読みちゃん読み上げタスク待受にTCP通信を使用します。  
+**IPAddress：127.0.0.1**  
+**Port：50005**  
+※現在固定値です。設定で変更できるように改善予定。  
+  
+上記アドレスに読み上げ指示を送信して下さい。  
+送信内容はBouyomiChanのSampleSrcに同梱されている「Socket通信で読み上げ指示を送る」を参考にして下さい。  
+  
+## Install
+「Plugin_BRBouyomi.dll」を「BouyomiChan.exe」と同フォルダに配置。  
+  
+## Uninstall
+「BouyomiChan.exe」と同フォルダに配置された「Plugin_BRBouyomi.dll」を削除。  
+  
+## Requirement
+[棒読みちゃん](http://chi.usamimi.info/Program/Application/BouyomiChan/)専用。  
+  
+## Related
+#### [Voiceroid Talk Plus](https://ch.nicovideo.jp/Wangdora/blomaga/ar12646)
+Voiceroidとの連携に使用。  
+#### [Voiceroid](https://www.ah-soft.com/product/series.html#voiceroid)
+Voiceroid本体。あかりちゃんかわいい。  
+### Extra
+#### [DiSpeak](https://github.com/micelle/dc_DiSpeak)
+Discordとの連携に使用。  
+  
+## Licence
+This software is released under the MIT License, see LICENSE.  
+  
+## Author
+[makiryk](https://github.com/makiryk)  
